9353673751cdb9df0f6fda3a9544206f81cb722e
Nhu <gaococonut1199@gmail.com>
2025-05-01 18:28:44 +0700

    Add QLLichThi QLKhachHang

---
+++ ACCI_CertificationExaminationCenter/UserControl/QuanLyLichThi.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace ACCI_CertificationExaminationCenter
+{
+    partial class QuanLyLichThi
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // QuanLyLichThi
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            Margin = new Padding(0);
+            Name = "QuanLyLichThi";
+            Size = new Size(1626, 1169);
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
